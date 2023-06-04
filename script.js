a3dd078983a7a626bf3734e0cafdc6e00720387c
bintadam <zakiyaadan12@gmail.com>
2023-06-04 16:06:40 +0200

    fetched data from the countries api

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const btnLanguage = document.querySelector(".btn-language")
 const countriesAPI = 'https://restcountries.com/v2/all'
 let arr = []
 
+const fectchApi = fetch(countriesAPI)
+.then(Response => Response.json())
+.then(data => {
+    let countriesData =  data;
+    countriesData.map(country => {
+        console.log(country)
+        arr.push(country)
+    })
+    // console.log(arr)
+})
+.catch(error => console.error(error)
+)
+
 const population = () => {
 
 }
