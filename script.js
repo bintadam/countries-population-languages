908ddf8f7aa2dd708e80ce8e095f72765fe86fab
bintadam <zakiyaadan12@gmail.com>
2023-06-04 16:44:39 +0200

    fixed error

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const btnPopulation = document.querySelector(".btn-population")
-const btnLanguage = document.querySelector(".btn-language")
+const btnPopulation = document.querySelector(".btn-population");
+const btnLanguage = document.querySelector(".btn-language");
 
-const countriesAPI = 'https://restcountries.com/v2/all'
-let arr = []
+const countriesAPI = 'https://restcountries.com/v2/all';
+
+let arr = [];
 
 const fectchApi = fetch(countriesAPI)
-.then(Response => Response.json())
-.then(data => {
-    let countriesData =  data;
-    countriesData.map(country => {
-        console.log(country)
-        arr.push(country)
+    .then(Response => Response.json())
+    .then(data => {
+        let countriesData =  data;
+        countriesData.map(country => { 
+            arr.push(country)
+        })
     })
-    // console.log(arr)
-})
-.catch(error => console.error(error)
+    .catch(error => console.error(error)
 )
+console.log(arr)
 
 const population = () => {
-
+    
 }
 
 population()
