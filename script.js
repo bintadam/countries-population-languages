51550b3dd4ac185f66b4aac2ada46238dae16fa6
bintadam <zakiyaadan12@gmail.com>
2023-06-05 14:14:42 +0200

    created sets

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const countryData = async () => {
     try{
         const response = await fetch(countriesAPI);
         const countries =  await response.json()
-        // console.log(countries)
-        let countryLanguage = [];
+    
+        let names = [];
         
         const targetCountries = countries.map(country => {
             return {
@@ -19,16 +19,42 @@ const countryData = async () => {
         })
 
         const highestPopulatedCountry =  targetCountries.sort((a,b) => b.population-a.population)
-        console.log(highestPopulatedCountry)
+        const tenMostPopulatedCountries = highestPopulatedCountry.slice(0,10)
+        console.log(tenMostPopulatedCountries)
+
 
         btnPopulation.addEventListener("click", function(e){
 
         })
 
+
         const targetLanguages = countries.map(country => {
+            let countryLanguage = [];
             countryLanguage.push(country.languages)
+            for(let i =0; i<countryLanguage.length; i++){
+                const innerArray = countryLanguage[i];
+                for(let n = 0; n<innerArray.length; n++){
+                    const obj = innerArray[n]
+                    const name = obj.name;
+                    names.push(name)
+                }
+            }
+            
         })
-        
+        // console.log(names)
+
+        const langset = new Set(names)
+        console.log()
+        const counts = [];
+        const count = {};
+
+        for(const language of langset){
+            const filteredLng = names.filter((lng)=> lng === language);
+            console.log(filteredLng)
+            counts.push({lang:language, count:filteredLng.length})
+        }
+        console.log(counts)
+      
 
     } catch(err){
         console.error(err)
